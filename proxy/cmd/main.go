8a8229461bbddad762659e7cee4452e6e7c2a896
padrinoexperto@mail.ru <you@example.com>
2025-02-04 15:42:42 +0700

    1

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"log"
-	"net"
-	"net/rpc"
 	_ "proxy/docs"
 	"proxy/internal/controller"
-	"proxy/internal/model"
 
 	servicer "proxy/internal/service"
 )
@@ -25,55 +21,12 @@ func main() {
 	r := controller.NewRouter(&handler)
 	server := controller.NewServer(":8080", r)
 
-	go func() {
-
-		server.Serve()
-	}()
-	go func() {
-		rpcService := &servicer.RPCServicer{Service: service}
-		rpc.Register(rpcService)
-
-		l, err := net.Listen("tcp", ":1234")
-		if err != nil {
-			log.Fatal("Ошибка при запуске сервера:", err)
-		}
-
-		log.Println("Сервер запущен на порту 1234")
-		rpc.Accept(l)
-	}()
+	go server.Serve()
+	go servicer.RPCServe(service)
 
 	//клиент для теста
-	go clientgo()
-	go clientgo()
+	go servicer.Clientgo()
+	go servicer.Clientgo()
 
 	server.Shutdown()
-}
-func clientgo() {
-	client, err := rpc.Dial("tcp", "localhost:1234")
-	if err != nil {
-		log.Fatal("Ошибка при подключении к RPC-серверу:", err)
-	}
-	defer client.Close()
-
-	// Пример вызова метода AddressSearch
-	req := model.RequestAddressSearch{Query: "Москва"}
-	var res model.ResponseAddress
-	err = client.Call("RPCServicer.AddressSearch", req, &res)
-	if err != nil {
-		log.Fatal("Ошибка при вызове метода AddressSearch:", err)
-	}
-	for _, addr := range res.Addresses {
-		log.Printf("Адрес: %+v\n", addr)
-	}
-
-	// Пример вызова метода GeoCode
-	geoReq := model.RequestAddressGeocode{Lat: "55.7558", Lon: "37.6173"}
-	var geoRes model.ResponseAddress
-	err = client.Call("RPCServicer.GeoCode", geoReq, &geoRes)
-	if err != nil {
-		log.Fatal("Ошибка при вызове метода GeoCode:", err)
-	}
-	for _, addr := range geoRes.Addresses {
-		log.Printf("Адрес: %+v\n", addr)
-	}
 }
--- proxy/internal/service/rpc.go
+++ proxy/internal/service/rpc.go
@@ -1,9 +1,24 @@
 package service
 
-import "proxy/internal/model"
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"proxy/internal/model"
+)
 
+type GeoProvider interface {
+	AddressSearch(req model.RequestAddressSearch, res *model.ResponseAddress) error
+	GeoCode(req model.RequestAddressGeocode, res *model.ResponseAddress) error
+}
 type RPCServicer struct {
 	Service Service
+}
+
+func NewGeoProvider(service Service) GeoProvider {
+	return &RPCServicer{
+		Service: service,
+	}
 }
 
 func (r *RPCServicer) AddressSearch(req model.RequestAddressSearch, res *model.ResponseAddress) error {
@@ -22,4 +37,46 @@ func (r *RPCServicer) GeoCode(req model.RequestAddressGeocode, res *model.Respon
 	}
 	res.Addresses = addresses
 	return nil
+}
+
+func RPCServe(service Service) {
+	rpcService := NewGeoProvider(service)
+	rpc.Register(rpcService)
+
+	l, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("Ошибка при запуске сервера:", err)
+	}
+
+	log.Println("Сервер запущен на порту 1234")
+	rpc.Accept(l)
+}
+func Clientgo() {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("Ошибка при подключении к RPC-серверу:", err)
+	}
+	defer client.Close()
+
+	// Пример вызова метода AddressSearch
+	req := model.RequestAddressSearch{Query: "Москва"}
+	var res model.ResponseAddress
+	err = client.Call("RPCServicer.AddressSearch", req, &res)
+	if err != nil {
+		log.Fatal("Ошибка при вызове метода AddressSearch:", err)
+	}
+	for _, addr := range res.Addresses {
+		log.Printf("Адрес: %+v\n", addr)
+	}
+
+	// Пример вызова метода GeoCode
+	geoReq := model.RequestAddressGeocode{Lat: "55.7558", Lon: "37.6173"}
+	var geoRes model.ResponseAddress
+	err = client.Call("RPCServicer.GeoCode", geoReq, &geoRes)
+	if err != nil {
+		log.Fatal("Ошибка при вызове метода GeoCode:", err)
+	}
+	for _, addr := range geoRes.Addresses {
+		log.Printf("Адрес: %+v\n", addr)
+	}
 }
